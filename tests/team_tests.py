b5a098a9dc02870028266b42aedd4f4648f6ce21
Daniel <dannyseeless@gmail.com>
2013-08-08 17:24:20 -0700

    Changed test to make sure unordered lists are same work

--- tests/team_tests.py
+++ tests/team_tests.py
@@ -49,4 +49,4 @@ class TeamTests(unittest.TestCase):
     def test_add_team_member(self):
         self.dummy.add_team_members("Daniel")
         self.dummy.add_team_members("Anna", "Heather", "Kimberly")
-        self.assertListEqual(self.dummy.get_team_members(), ["Daniel", "Anna", "Heather", "Kimberly"])
+        self.assertListEqual(self.dummy.get_team_members(), ["Daniel", "Heather", "Anna", "Kimberly"])
