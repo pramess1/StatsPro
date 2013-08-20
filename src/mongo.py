0f803df080a29eaf17aaba2e023eef15575fbd43
Daniel <dannyseeless@gmail.com>
2013-08-20 14:12:19 -0700

    Continued on mongo.py

--- src/mongo.py
+++ src/mongo.py
@@ -6,36 +6,35 @@
 import pymongo
 import datetime
 
-class pymongodb(object):
+class MongoDB(object):
     def __init__(self):
     	self.database
         self.person
         self.round_
+        self.date = datetime.date.today()
 
-    def connect_to_mongo(self):
+    def connect_to_mongo_database(self):
         client = MongoClient("localhost", 27017)
-        database = client.statpro
+        self.database = client.StatsPro
 
-    def post_person(self, person, round_, *tags, date):
+    def initial_post_person(self, person, round_):
         post = {"person"  : person,
                 "round"   : round_,
-                "tags"    : tags
-                "date"    : date
+                "date"    : datetime.date.today()
                 }
-        person = database.person
+        self.person = database.person
         person_pers = person.insert(person)
 
-    def post_round(self):
+    def initial_post_team(self, team, round_):
         post = {"team"    : team,
-                "members" : members
-                "tags"    : tags
-                "date"    : datetime
+                "members" : members,
+                "date"    : datetime.date.today()
                 }
         self.round_ = database.round_
-        round_rond = self.round_.insert(round)
+        round_rond = self.round_.insert(round_)
 
-    def search_person(self):
-        person.find_one({"person": person_pers})
+    def search_person(self, name):
+        person.find_one({"person": name})
 
-    def search_round(self):
-        round_.find_one({"round": round_rond})
+    def search_round(self, number):
+        round_.find_one({"round": number})
