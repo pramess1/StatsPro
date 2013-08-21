cfa2be444c7da1b956cb3e620550814870dca9ad
Daniel <dannyseeless@gmail.com>
2013-08-20 17:26:28 -0700

    Made more improvements

--- src/team.py
+++ src/team.py
@@ -7,54 +7,22 @@ class Team(object):
     def __init__(self):
         self._name = ""
         self._round = {}
-        self._team_members = []
-        self._opposing_team = {}
-        self._team_scores = {}
-        self._team_wins_losses_round = {}
 
-    """Returns the name of the team.
-    Returns the name of the team.
-    Returns:
-        String: the name of the team as a string.
-    """
     def get_team_name(self):
         return self._name
 
-    """Sets the name of the team.
-    Simply sets the name of the team.
-    Args:
-        String: the name of the team as a string.
-    """
     def set_team_name(self, name):
         self._name = name
 
-    """Adds a round to the team.
-    Adds a round with all data to the current team.
-    Args:
-        Integer: the round number
-        Integer: the round score
-        String: the name of the opposing team
-        Boolean: whether the round was a win or loss
-    """
     def add_round(self, round_, score, errors, opponent, is_win):
         if is_win == True:
             self._round[round_] = [score, errors, opponent, "W"]
         else:
             self._round[round_] = [score, errors, opponent, "L"]
 
-    """Removes a round from the current team.
-    Removes a round and all data from the current team.
-    Args:
-        Integer: the round number to delete.
-    """
     def delete_round(self, round_):
         del self._round[round_]
 
-    """Gets a round.
-    Returns a round that is passed to the function.
-    Args:
-        round_: integer
-    """
     def get_round(self, round_):
         return self._round[round_]
 
