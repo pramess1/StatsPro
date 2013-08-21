b4daf6944facb9075e435f1ac5dc509796c37ac0
Daniel <dannyseeless@gmail.com>
2013-08-20 17:48:17 -0700

    Added functions to person module

--- src/person.py
+++ src/person.py
@@ -8,5 +8,34 @@ class Person(object):
 
     def __init__(self):
         self.name = ""
+        self.round = {}
 
-    
+    def get_person_name(self):
+        return self._name
+
+    def set_person_name(self, name):
+        self._name = name
+
+    def add_round(self, round_, score, errors):
+        if is_win == True:
+            self._round[round_] = [score, errors]
+        else:
+            self._round[round_] = [score, errors]
+
+    def delete_round(self, round_):
+        del self._round[round_]
+
+    def get_round(self, round_):
+        return self._round[round_]
+
+    def change_round_score(self, round_, new_score):
+        self._round[round_][0] = new_score
+
+    def change_round_errors(self, round_, new_errors):
+        self._round[round_][1] = new_errors
+
+    def get_round_score(self, round_):
+        return self._round[round_][0]
+
+    def get_round_errors(self, round_):
+        return self._round[round_][1]
